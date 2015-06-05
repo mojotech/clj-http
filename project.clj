e6f8e656c521d05421f7ccbd3d03f6e21f4d718b
Will Farrell <will@mojotech.com>
2015-06-05 14:48:30 -0400

    Version 2.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wkf/clj-http "2.0.0"
+(defproject wkf/clj-http "2.0.1-SNAPSHOT"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/mojotech/clj-http/"
   :license {:name "The MIT License"
